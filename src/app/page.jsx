b3baf43addca61da7d922acb719abb9604da4a4d
Kim-jules <jehovahjules@gmail.com>
2025-09-09 17:44:07 +0200

    adding the shared folder in the components.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@/components/layouts/button";
+import { Button } from "@/components/shared/button";
 
 export default function Home() {
   return (
