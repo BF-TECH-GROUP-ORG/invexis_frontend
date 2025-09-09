d03d97778f32b06912ebc19b28cc4becc9a4f38e
Kim-jules <jehovahjules@gmail.com>
2025-09-09 18:12:49 +0200

    Removed a bug.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,6 @@
-"use client";
-
-import { Provider } from "react-redux";
 import store from "@/store";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import ClientProviders from "./ClientProviders";
 
 export const metadata = {
   title: "Create Next App",
@@ -23,12 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Provider store={store}>
-          {children}
-        </Provider>
+      <body className={`antialiased`}>
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   );
