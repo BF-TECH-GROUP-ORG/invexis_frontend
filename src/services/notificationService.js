2bfa5253cc3646908df5fb043d5a3a88c438a893
shinjagira <shinjagiraarnold209@gmail.com>
2025-12-19 11:25:27 +0200

    shops service form

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,6 +1,6 @@
 import apiClient from "@/lib/apiClient";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL + "/api/notifications";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL + "/notifications";
 
 /**
  * Get User Notifications
